d294604ca5fb4be56f091861412e6bd4daa9ef2d
Rebecca Keegan <rebeccachristinekeegan@gmail.com>
2019-04-12 10:59:39 +0100

    split, reversed, join names file. Created names-reversed file

--- part-1.js
+++ part-1.js
@@ -1,5 +1,26 @@
 var fs = require('fs')
 
-var fileContents = fs.readFileSync('names.txt', 'utf-8')
+var fileContents = fs.readFileSync('./names.txt', 'utf-8')
+var textByLine = fileContents.split('\n')
+var reversedArr = textByLine.reverse();
 
-console.log(fileContents)
+function reverse(textByLine) {
+    var temp = []
+    var len = textByLine.length
+    for (var i = (len-1); i >= 0; i--) {
+        temp.push(textByLine[i]);
+    }
+    return temp;
+}
+
+var reversedArr = reverse(textByLine)
+var joinedReversed = ""
+for (i =0; i < reversedArr.length; i++) {
+    joinedReversed += (reversedArr[i] + "\n")
+}
+
+fs.writeFileSync('names-reversed', joinedReversed, 'utf-8')
+
+console.log(textByLine)
+console.log(reversedArr)
+console.log(joinedReversed)
